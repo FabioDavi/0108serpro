aa9e6bb5780b6e3a373338773b4a660624f4e84c
Dev-js <fdr.desenvolvedor@gmail.com>
2023-08-01 08:49:12 -0300

    e

---
+++ test.mjs
@@ -0,0 +1,15 @@
+import assert from 'assert';
+import { bank } from './api.mjs'
+
+function soma(a, b) {
+    return a + b;
+}
+
+
+const saldo = bank.operation.sacar(38, 35);
+console.log('saldo ' + saldo);
+assert.equal(soma(2, 2), 4);
+
+//assert.equal(bank.operation.sacar(38, 35), 3);
+
+console.log(bank.transacao);
